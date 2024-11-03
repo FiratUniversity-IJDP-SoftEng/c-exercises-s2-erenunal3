fc351c158eee01397dda06b691b3e0c873c8caf8
eren unal <162706109+erenunal3@users.noreply.github.com>
2024-11-03 15:02:16 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int main() {
+    int number; 
+    int count = 0; 
+    int sum = 0;
+    printf("enter numbers(run by typing -1):\n");
+    while (1) { 
+        scanf("%d", &number); 
+        if (number == -1) { 
+            break;
+        }
+        count++; 
+        sum += number;
+    }
+    if (count > 0) {
+        float average = (float)sum / count; 
+        printf("number of numbers entered %d\n", count); 
+        printf("total: %d\n", sum); 
+        printf("avarage: %.2f\n", average);
+    } else {
+        printf("nothing entered.\n"); 
+    }
+}
