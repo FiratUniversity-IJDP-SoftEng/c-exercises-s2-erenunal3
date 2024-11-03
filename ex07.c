680338c07365de4b1da7b368256c283a33c7e05d
eren unal <162706109+erenunal3@users.noreply.github.com>
2024-11-03 13:54:50 +0300

    siu

--- ex07.c
+++ ex07.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        int col1 = i * 5;                               
+        int col2 = 100 - ((i - 1) * 10 + 1);           
+        int col3 = i;                                 
+
+        printf("%d\t%d\t%d\t", col1, col2, col3); 
+    }
+}
 
