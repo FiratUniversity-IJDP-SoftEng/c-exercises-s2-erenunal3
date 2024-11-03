d2c56bc05d8e26ea824d2f503da4e305a0712a77
eren unal <162706109+erenunal3@users.noreply.github.com>
2024-11-03 15:16:11 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main() {
+    int size = 5; 
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) { 
+            if (i == j || i + j == size - 1) {
+                printf("*"); 
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n"); 
+    }
+}
