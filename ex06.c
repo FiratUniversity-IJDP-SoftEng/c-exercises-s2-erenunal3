f4c698852f269e6c11059ed9d94977c3c0f1dcea
eren unal <162706109+erenunal3@users.noreply.github.com>
2024-11-03 14:44:02 +0300

    a

--- ex06.c
+++ ex06.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[20]; 
+    int index = 0;    
+    for (int i = 99; i >= 51; i -= 3) {
+        numbers[index] = i; 
+        index++;             
+    }
+    printf("3'e bölünebilen sayılar (100'den 50'ye):\n");
+    for (int j = 0; j < index; j++) {
+        printf("%d\n", numbers[j]);  
+    }
+}
