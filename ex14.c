a39a056fecddf6f4f04eea6206306af6f7e466f3
eren unal <162706109+erenunal3@users.noreply.github.com>
2024-11-03 16:38:26 +0300

    Update ex14.c

--- ex14.c
+++ ex14.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n; 
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+int main() {
+    int n;
+    printf("what position do you want to see the fibonacci series? ");
+    scanf("%d", &n);
+    printf("fibonacci series (first number):\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+    printf("%d. Fibonacci number: %d", n, fibonacci(n-1));
+}
